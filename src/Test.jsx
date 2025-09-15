0afd35754057026ca2a8f5c81135b09a86d7e375
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-15 17:02:59 +0600

    Setting Classes and Text Conditionally

--- src/Test.jsx
+++ src/Test.jsx
@@ -4,42 +4,42 @@ const pizzaData = [
     name: 'Focaccia',
     ingredients: 'Bread with italian olive oil and rosemary',
     price: 6,
-    photoName: 'pizzas/focaccia.jpg',
+    photoName: '/assets/focaccia.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Margherita',
     ingredients: 'Tomato and mozzarella',
     price: 10,
-    photoName: 'pizzas/margherita.jpg',
+    photoName: '/assets/margherita.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Spinaci',
     ingredients: 'Tomato, mozzarella, spinach, and ricotta cheese',
     price: 12,
-    photoName: 'pizzas/spinaci.jpg',
+    photoName: '/assets/spinaci.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Funghi',
     ingredients: 'Tomato, mozzarella, mushrooms, and onion',
     price: 12,
-    photoName: 'pizzas/funghi.jpg',
+    photoName: '/assets/funghi.jpg',
     soldOut: false,
   },
   {
     name: 'Pizza Salamino',
     ingredients: 'Tomato, mozzarella, and pepperoni',
     price: 15,
-    photoName: 'pizzas/salamino.jpg',
+    photoName: '/assets/salamino.jpg',
     soldOut: true,
   },
   {
     name: 'Pizza Prosciutto',
     ingredients: 'Tomato, mozzarella, ham, arugula, and burrata cheese',
     price: 18,
-    photoName: 'pizzas/prosciutto.jpg',
+    photoName: '/assets/prosciutto.jpg',
     soldOut: false,
   },
 ];
@@ -58,20 +58,24 @@ const Menu = function () {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven, all organic, all delicious.
-      </p>
+
       {/* conditional rendering with ternary */}
       {pizzasLength > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map(pizza => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu please come back later. ☺️</p>
       )}
+
       {/* conditional rendering with and 
       {pizzasLength > 0 && (
         <ul className="pizzas">
@@ -107,50 +111,50 @@ const Menu = function () {
   );
 };
 //importing all images focaccia এখন একটা variable, যার ভ্যালু হলো ওই ইমেজের path
-import focaccia from './assets/focaccia.jpg';
-import margherita from './assets/margherita.jpg';
-import spinaci from './assets/spinaci.jpg';
-import funghi from './assets/funghi.jpg';
-import salamino from './assets/salamino.jpg';
-import prosciutto from './assets/prosciutto.jpg';
+//import focaccia from './assets/focaccia.jpg';
+// import margherita from './assets/margherita.jpg';
+// import spinaci from './assets/spinaci.jpg';
+// import funghi from './assets/funghi.jpg';
+// import salamino from './assets/salamino.jpg';
+// import prosciutto from './assets/prosciutto.jpg';
 
 //making an image object এটা basically একটা lookup table বানানো হয়েছে — যাতে photoName (string) আর import করা variable (value) এর মধ্যে mapping থাকে:
-const images = {
-  'pizzas/focaccia.jpg': focaccia,
-  'pizzas/margherita.jpg': margherita,
-  'pizzas/spinaci.jpg': spinaci,
-  'pizzas/funghi.jpg': funghi,
-  'pizzas/salamino.jpg': salamino,
-  'pizzas/prosciutto.jpg': prosciutto,
-};
+// const images = {
+//   //'pizzas/focaccia.jpg': focaccia,
+//   'pizzas/margherita.jpg': margherita,
+//   'pizzas/spinaci.jpg': spinaci,
+//   'pizzas/funghi.jpg': funghi,
+//   'pizzas/salamino.jpg': salamino,
+//   'pizzas/prosciutto.jpg': prosciutto,
+// };
 
-//giving the parameter props which will take the values passed in the pizzas
-const Pizza = function (props) {
-  console.log(props); //like an object
-  if (props.pizzaObj.soldOut) return null;
+//ei vabe na kore sorasori tmr sob image gula ke public folder e transfer kore dao
+
+//giving the parameter props which will take the values passed in the pizzas with destructure
+const Pizza = function ({ pizzaObj }) {
+  //console.log(pizzaObj); //like an object
+  const isSoldOut = pizzaObj.soldOut
+    ? `SOLD OUT`
+    : `Price:  $${pizzaObj.price}`;
+
   return (
-    <li className="pizza">
-      <img src={images[props.pizzaObj.photoName]} alt={props.pizzaObj.name} />
+    <li className={`${pizzaObj.soldOut ? 'pizza sold-out' : 'pizza'}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>Price: ${props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{isSoldOut}</span>
       </div>
     </li>
   );
 };
 
 const Footer = function () {
-  const restaurantOpen = () => {
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closeHour = 22;
-    const isOpen =
-      hour >= openHour && hour <= closeHour
-        ? `We are currently open until ${closeHour}:00. Come visit us or order online.`
-        : `Sorry! We are closed now. We're open from ${openHour}:00 to ${closeHour}:00. Come visit us or order online.`;
-    return isOpen;
-  };
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   // const hour = new Date().getHours();
   // const openHour = 12;
   // const closeHour = 22;
@@ -159,22 +163,49 @@ const Footer = function () {
   // const anotherStyle = { color: 'red', fontSize: '20px' };
   return (
     <footer className="footer">
-      <div className="order">
-        {/* {isOpen && (
+      {isOpen ? (
+        <Open closeHour={closeHour} />
+      ) : (
+        <Close closeHour={closeHour} openHour={openHour} />
+      )}
+      {/*<div className="order">
+         {isOpen && (
         <div className="order">
           <p>Open</p>
           <button className="btn">Order</button>
         </div>
-      )} */}
-        <p>{restaurantOpen()}</p>
-        {/* <p style={{ color: 'red', fontSize: '20px' }}>
+      )} 
+      <p>{restaurantOpen()}</p>
+      {/* <p style={{ color: 'red', fontSize: '20px' }}>
           We're open from 12:00 to 22:00. Come visit us or order online.
         </p> */}
-        {/* <p style={anotherStyle}>Lorem ipsum dolor sit amet.</p> */}
+      {/* <p style={anotherStyle}>Lorem ipsum dolor sit amet.</p> 
         <button className="btn">Order</button>
-      </div>
+      </div>*/}
+      <button className="btn">Order</button>
     </footer>
   );
 };
 
+const Open = function ({ closeHour }) {
+  // console.log(closeHour);
+  return (
+    <div className="order">
+      <p>
+        We are currently open until {closeHour}:00. Come visit us or order
+        online.
+      </p>
+    </div>
+  );
+};
+const Close = function ({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        Sorry! We are closed now. We're open from {openHour}:00 to {closeHour}
+        :00. Come visit us or order online.
+      </p>
+    </div>
+  );
+};
 export { Header, Menu, Footer };
